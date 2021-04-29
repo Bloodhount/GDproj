d097447d5670ec1c5816004d3d7d3aef8ab49644
Bloodhount <stanislavviktorovich2016@gmail.com>
2021-04-29 20:32:42 +0300

    progress in myAdvancedGUI

--- Assets/Scripts/GUI scripts/myAdvancedGUI.cs
+++ Assets/Scripts/GUI scripts/myAdvancedGUI.cs
@@ -5,19 +5,43 @@ public class myAdvancedGUI : MonoBehaviour
 {
     [SerializeField] [Header("tune var")] [Range(0, 100)] [Tooltip("some info")] private float mySlider = 1.0f;
     [SerializeField] [TextArea(5, 10)] private string my2Slider;
-    [SerializeField]private int my3Slider = 1;
+    [SerializeField] private int my3Slider = 1;
 
     public Color myColor;
     public MeshRenderer GO;
 
     void OnGUI()
     {
-        mySlider = LebelSlider(new Rect(10, 10, 200, 20), mySlider, 5.0f, "My Slider");
-
+        mySlider = LebelSlider(new Rect(10, 10, 200, 30), mySlider, 100.0f, "My Slider");
+        myColor = RGBSlider(new Rect(10, 60, 200, 30), myColor);
+        GO.material.color = myColor;
     }
 
-    float LebelSlider(Rect rect, float mySlider, float v1, string v2)
+    float LebelSlider(Rect screenRect, float sliderValue, float sliderValueMax, string lebelText)
     {
-        throw new NotImplementedException();
+        Rect lebelRect = new Rect(screenRect.x, screenRect.y, screenRect.width, screenRect.height);
+
+        GUI.Label(lebelRect, lebelText);
+
+        Rect sliderRect = new Rect(screenRect.x + screenRect.width/2, screenRect.y, screenRect.width/2, screenRect.height);
+        sliderValue = GUI.HorizontalSlider(sliderRect, sliderValue, 0.0f, sliderValueMax);
+
+        return sliderValue;
+    }
+
+    private Color RGBSlider(Rect screenRect, Color myRGBColor)
+    {
+        myRGBColor.r = LebelSlider(screenRect, myRGBColor.r, 1f, "Red");
+
+        screenRect.y += 40;
+        myRGBColor.g = LebelSlider(screenRect, myRGBColor.g, 1f, "Green");
+
+        screenRect.y += 40;
+        myRGBColor.b = LebelSlider(screenRect, myRGBColor.b, 1f, "Blue");
+
+        screenRect.y += 40;
+        myRGBColor.a = LebelSlider(screenRect, myRGBColor.a, 1f, "Alpha");
+
+        return myRGBColor;
     }
 }
